e81be4c6ad88b684fea85ebbc5e0c3675aec5124
An Pham <anpham148@gmail.com>
2020-06-15 09:42:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board1)
   counter = 0
   board1.each do |x|
-    counter += 1
+    if board1[counter] == "X" or board1[counter] == "O"
+      counter += 1
+    end
     return counter
   end
 end
