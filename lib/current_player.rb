41ac597dee70096400e5eb5483088cf6b7de90e4
An Pham <anpham148@gmail.com>
2020-06-15 09:43:33 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board1)
   counter = 0
   board1.each do |space|
-    if space == "X" or space == "O"
+    if space == "X" || space == "O"
       counter += 1
     end
     return counter
